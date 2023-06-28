4753928081e8880f1f31df3c02421d305716336d
Jasper Halvorson <114035580+Jaspvr@users.noreply.github.com>
2023-06-27 22:32:09 -0700

    scrape
    first scrape that actually works, used a virtual machine

---
+++ dataScaper.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
+plt.style.use('seaborn')
+
+import yfinance as yf
+
+msft = yf.Ticker('msft')
+
+print(msft.info)
+
+
+
+
+
+
+
+
+
+
+
+# IS NOT WORKING
+# import math
+# import pandas_datareader as web
+# import numpy as np
+# import pandas as pd
+# from sklearn.preprocessing import MinMaxScaler
+# from keras.models import Sequential
+# from keras.layers import Dense, LSTM
+# import matplotlib.pyplot as plt
+# import tensorflow as tf
+# plt.style.use('fivethirtyeight')
+
+# #Get stock quote
+# df = web.DataReader('AAPL', data_source='yahoo', start='2012-01-01', end='2019-12-17')
+# #Show the data
+# df
+print("hello")
+
+
+
